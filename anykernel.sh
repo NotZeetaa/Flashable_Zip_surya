574932a96b6da90f5f3c2ebb09abab63aefb8d7d
NotZeetaa <rodrigo2005contente@gmail.com>
2021-06-27 14:53:02 +0100

    bringup to surya
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,28 +4,45 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neXus_kernel_lmi
-do.devicecheck=0
+kernel.string=neXus_kernel_surya
+do.devicecheck=1
 do.modules=0
-do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lmi
-device.name2=lmipro
-supported.versions=11
+device.name1=surya
+device.name2=karna
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+
 ## AnyKernel install
 dump_boot;
 
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
 write_boot;
 ## end install
+
